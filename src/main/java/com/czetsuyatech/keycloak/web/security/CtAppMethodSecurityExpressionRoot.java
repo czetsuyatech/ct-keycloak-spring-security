1feb58b93d8cd37633d715e58a7c2c266f1ca876
Edward P. Legaspi <czetsuya@gmail.com>
2022-10-12 18:06:18 +0800

    first commit

---
+++ src/main/java/com/czetsuyatech/keycloak/web/security/CtAppMethodSecurityExpressionRoot.java
@@ -0,0 +1,29 @@
+package com.czetsuyatech.keycloak.web.security;
+
+import com.czetsuyatech.spring.security.method.DefaultMethodSecurityExpressionRoot;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.Authentication;
+
+public class CtAppMethodSecurityExpressionRoot extends DefaultMethodSecurityExpressionRoot {
+
+  public CtAppMethodSecurityExpressionRoot(Authentication authentication) {
+    super(authentication);
+  }
+
+
+  public boolean isAuthorized() {
+    return true;
+  }
+
+  public boolean isUnAuthorized() {
+    return false;
+  }
+
+  @Builder
+  @Data
+  public static class UserData {
+
+    private final Long id;
+  }
+}
---
+++ src/main/java/com/czetsuyatech/keycloak/web/security/CtAppSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.czetsuyatech.keycloak.web.security;
+
+import com.czetsuyatech.spring.security.EnableCtSecurity;
+import com.czetsuyatech.spring.security.method.CtMethodSecurityExpressionHandlerFactory;
+import com.czetsuyatech.spring.security.web.CtHttpSecurityConfigurer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+/**
+ * @author Edward P. Legaspi | czetsuya@gmail.com
+ */
+@Configuration
+@RequiredArgsConstructor
+@EnableCtSecurity
+public class CtAppSecurityConfiguration {
+
+  @Bean
+  public CtHttpSecurityConfigurer httpSecurityConfig() {
+
+    return http ->
+        http
+            .authorizeHttpRequests(authz -> authz
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+            );
+  }
+
+  @Bean
+  public CtMethodSecurityExpressionHandlerFactory methodSecurityFactory() {
+    return CtAppMethodSecurityExpressionRoot::new;
+  }
+}
